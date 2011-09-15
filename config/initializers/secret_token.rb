b9e922727a5190455488d4ed886b971f42ca0063
sudohack <andreimpop@gmail.com>
2011-09-15 10:37:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1621b92559a618ea0580797b0c1fdb61a734a62cba05f40d4a518b9222596c40ea1051a6282f02c410c0b4166df00ec383437fbd1854ead95d5e0b797286d83e'
